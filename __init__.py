6626e0da48767d64f158800273d65169236473e1
PiloeGAO <leonumerique@gmail.com>
2018-09-25 19:10:16 +0200

    Multifiles support!
    The original "__init__.py" was changed in "modules_installer_ui.py" to help devs to add this new sub-addon in their projects.

--- __init__.py
+++ __init__.py
@@ -1,150 +1,61 @@
+'''
+Copyright (C) 2018 Leo DEPOIX
+LEONUMERIQUE@GMAIL.com
+
+Created by Leo DEPOIX
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 bl_info = {
-    "name": "Module Installer",
-    "author": "PiloeGAO",
-    "version": (0, 1),
+    "name": "Modules Installer",
+    "description": "",
+    "author": "PiloeGAO (Leo DEPOIX)",
+    "version": (0, 0, 1),
     "blender": (2, 79, 0),
-    "location": "SpaceBar Search -> Add-on Preferences",
-    "description": "Installing new modules to blender",
-    "warning": "In Dev",
+    "location": "",
+    "warning": "This addon is still in development.",
     "wiki_url": "",
-    "tracker_url": "",
-    "category": "System",
-    }
-
-import bpy, subprocess, sys
-from bpy.types import Operator, AddonPreferences
-from bpy.props import StringProperty
-
-class UIAddonPreferences(AddonPreferences):
-    # this must match the add-on name, use '__package__'
-    # when defining this in a submodule of a python package.
-    bl_idname = __name__
-
-    python_filepath = StringProperty(
-            name="Python File Path (blender python exec)",
-            subtype='FILE_PATH',
-            default=sys.executable, #TO TEST > Not working on MACOS
-            ) #Python file path
-
-    pip_install_file = StringProperty(
-            name="PIP install file",
-            subtype='FILE_PATH',
-            default=bpy.utils.user_resource('SCRIPTS', path="addons/Module-Installer/utils/get-pip.py"), #Locate get-pip.py to install PIP
-            ) #PIP install file
-
-    pip_modules = StringProperty(
-            name="PIP modules (just space between modules)",
-            subtype='NONE',
-            ) #PIP modules neededs
-
-    def draw(self, context):
-        layout = self.layout
-        layout.label(text="This is a preferences view for our add-on")
-
-        box = layout.box()
-        box.prop(self, "python_filepath")
-        box.prop(self, "pip_install_file")
-        box.operator("system.install_pip")
-
-        box.prop(self, "pip_modules")
-        box.operator("system.install_modules")
-        box.operator("system.uninstall_modules")
+    "category": "System" }
 
 
-class SYSTEM_OT_addon_module_installer(Operator):
-    """Display example preferences"""
-    bl_idname = "system.addon_module_installer"
-    bl_label = "Add-on Module Installer"
-    bl_options = {'REGISTER', 'UNDO'}
+import bpy
 
-    def execute(self, context):
 
-        user_preferences = context.user_preferences
-        addon_prefs = user_preferences.addons[__name__].preferences
+# load and reload submodules
+##################################
 
-        return {'FINISHED'}
+import importlib
+from . import developer_utils
+importlib.reload(developer_utils)
+modules = developer_utils.setup_addon_modules(__path__, __name__, "bpy" in locals())
 
-class PIPInstaller(bpy.types.Operator): #PIP installer class
-    bl_idname = "system.install_pip"
-    bl_label = "Install PIP"
 
-    def execute(self, context):
-        user_preferences = context.user_preferences
-        addon_prefs = user_preferences.addons[__name__].preferences
 
-        print("Let's install PIP")
+# register
+##################################
 
-        command = subprocess.Popen(addon_prefs.python_filepath + " " + addon_prefs.pip_install_file, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) #Command to install PIP
-        
-        if command.wait() != 0:
-            output, error = command.communicate()
-            self.report({'ERROR'}, str(error))
-        else:
-            self.report({'INFO'}, "PIP installed successfully")
-        
-        return {'FINISHED'}
+import traceback
 
-class ModuleInstaller(bpy.types.Operator): #Module installer class
-    bl_idname = "system.install_modules"
-    bl_label = "Install Modules"
-
-    def execute(self, context):
-        user_preferences = context.user_preferences
-        addon_prefs = user_preferences.addons[__name__].preferences
-
-        print("Let's install modules")
-
-        python_dir = os.path.dirname(os.path.dirname(addon_prefs.python_filepath))
-        pip_location = python_dir + "\Scripts\pip.exe" #Only for windows (mac and linux later)
-
-        command = subprocess.Popen(addon_prefs.python_filepath + " " + pip_location + " install " + addon_prefs.pip_modules, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) #Command to install modules
-        
-        if command.wait() != 0:
-            output, error = command.communicate()
-            self.report({'ERROR'}, str(error))
-        else:
-            self.report({'INFO'}, "Modules installed successfully")
-        
-        return {'FINISHED'}
-
-class ModuleUninstaller(bpy.types.Operator): #Modle uninstaller class
-    bl_idname = "system.uninstall_modules"
-    bl_label = "Uninstall Modules"
-
-    def execute(self, context):
-        user_preferences = context.user_preferences
-        addon_prefs = user_preferences.addons[__name__].preferences
-
-        print("Let's uninstall modules")
-
-        python_dir = os.path.dirname(os.path.dirname(addon_prefs.python_filepath))
-        pip_location = python_dir + "\Scripts\pip.exe" #Only for windows (mac and linux later)
-
-        command = subprocess.Popen(addon_prefs.python_filepath + " " + pip_location + " uninstall " + addon_prefs.pip_modules, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) #Command to uninstall modules
-        
-        if command.wait() != 0:
-            output, error = command.communicate()
-            self.report({'ERROR'}, str(error))
-        else:
-            self.report({'INFO'}, "Modules uninstalled successfully")
-            
-        return {'FINISHED'}
-
-# Registration
 def register():
-    bpy.utils.register_class(SYSTEM_OT_addon_module_installer)
-    bpy.utils.register_class(UIAddonPreferences)
-    bpy.utils.register_class(PIPInstaller)
-    bpy.utils.register_class(ModuleInstaller)
-    bpy.utils.register_class(ModuleUninstaller)
+    try: bpy.utils.register_module(__name__)
+    except: traceback.print_exc()
 
+    print("Registered {} with {} modules".format(bl_info["name"], len(modules)))
 
 def unregister():
-    bpy.utils.unregister_class(SYSTEM_OT_addon_module_installer)
-    bpy.utils.unregister_class(UIAddonPreferences)
-    bpy.utils.unregister_class(PIPInstaller)
-    bpy.utils.unregister_class(ModuleInstaller)
-    bpy.utils.unregister_class(ModuleUninstaller)
+    try: bpy.utils.unregister_module(__name__)
+    except: traceback.print_exc()
 
-if __name__ == "__main__":
-    register()
+    print("Unregistered {}".format(bl_info["name"]))
---
+++ modules_installer_ui.py
@@ -0,0 +1,123 @@
+import bpy, subprocess, sys, os
+from bpy import context
+from bpy.types import Operator, AddonPreferences
+from bpy.props import StringProperty
+import importlib
+
+class UIAddonPreferences(AddonPreferences):
+    # this must match the add-on name, use '__package__'
+    # when defining this in a submodule of a python package.
+    bl_idname = __package__
+
+    python_filepath = StringProperty(
+            name="Python File Path (blender python exec)",
+            subtype='FILE_PATH',
+            default=sys.executable, #ONLY WORKING ON WINDOWS CURRENTLY
+            ) #Python file path
+
+    pip_install_file = StringProperty(
+            name="PIP install file",
+            subtype='FILE_PATH',
+            default=bpy.utils.user_resource('SCRIPTS', path="addons/Module-Installer/utils/get-pip.py"), #Locate get-pip.py to install PIP
+            ) #PIP install file
+
+    pip_filepath = StringProperty(
+            name="PIP File Path (blender python PIP)",
+            subtype='FILE_PATH',
+            default=sys.executable, #ONLY WORKING ON WINDOWS CURRENTLY
+            ) #Python file path
+
+    pip_modules = StringProperty(
+            name="PIP modules (just space between modules)",
+            subtype='NONE',
+            ) #PIP modules wanted by user
+
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text="This is preferences to setup PIP and new modules")
+
+        box = layout.box()
+        box.prop(self, "python_filepath")
+        box.prop(self, "pip_install_file")
+        box.operator("system.install_pip")
+
+        box.prop(self, "pip_modules")
+        box.prop(self, "pip_filepath")
+        box.operator("system.install_modules")
+        box.operator("system.uninstall_modules")
+
+
+class SYSTEM_addon_module_installer(Operator):
+    bl_idname = "system.addon_module_installer"
+    bl_label = "Add-on Module Installer"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        
+        user_preferences = context.user_preferences
+        addon_prefs = user_preferences.addons[__package__].preferences
+
+        return {'FINISHED'}
+
+class PIPInstaller(bpy.types.Operator): #PIP installer class
+    bl_idname = "system.install_pip"
+    bl_label = "Install PIP"
+
+    def execute(self, context):
+        user_preferences = context.user_preferences
+        addon_prefs = user_preferences.addons[__name__].preferences
+
+        print("Let's install PIP")
+
+        command = subprocess.Popen("\"" + addon_prefs.python_filepath +  " --python \" " + addon_prefs.pip_install_file, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) #Command to install PIP
+
+        if command.wait() != 0:
+            output, error = command.communicate()
+            self.report({'ERROR'}, str(error))
+        else:
+            self.report({'INFO'}, "PIP installed successfully")
+
+        return {'FINISHED'}
+
+class ModuleInstaller(bpy.types.Operator): #Modules installer class
+    bl_idname = "system.install_modules"
+    bl_label = "Install Modules"
+
+    def execute(self, context):
+        user_preferences = context.user_preferences
+        addon_prefs = user_preferences.addons[__name__].preferences
+
+        print("Let's install modules")
+
+        command = subprocess.Popen("\"" + addon_prefs.python_filepath + "\" --python " + "\"" + addon_prefs.pip_filepath + "\" install " + addon_prefs.pip_modules, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) #Command to install modules
+
+        if command.wait() != 0:
+            output, error = command.communicate()
+            self.report({'ERROR'}, str(error))
+        else:
+            self.report({'INFO'}, "Modules installed successfully")
+
+        return {'FINISHED'}
+
+class ModuleUninstaller(bpy.types.Operator): #Modules uninstaller class
+    bl_idname = "system.uninstall_modules"
+    bl_label = "Uninstall Modules"
+
+    def execute(self, context):
+        user_preferences = context.user_preferences
+        addon_prefs = user_preferences.addons[__name__].preferences
+
+        print("Let's uninstall modules")
+
+        python_dir = os.path.dirname(os.path.dirname(addon_prefs.python_filepath))
+        pip_location = python_dir + "\Scripts\pip.exe" #Only for windows (mac and linux later)
+
+        command = subprocess.Popen("\"" + addon_prefs.python_filepath + "\" " + "\"" + addon_prefs.pip_filepath + "\" uninstall " + addon_prefs.pip_modules, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) #Command to uninstall modules
+
+        if command.wait() != 0:
+            output, error = command.communicate()
+            self.report({'ERROR'}, str(error))
+        else:
+            self.report({'INFO'}, "Modules uninstalled successfully")
+
+        return {'FINISHED'}

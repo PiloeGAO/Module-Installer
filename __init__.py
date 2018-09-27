dbc0e172fca393dd98d1f5c627be531106898a60
PiloeGAO <leonumerique@gmail.com>
2018-09-27 11:21:04 +0200

    2.80 basic update
    Based on the official blender guidelines for 2.80 update.  Tests needed.

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ bl_info = {
     "description": "",
     "author": "PiloeGAO (Leo DEPOIX)",
     "version": (0, 0, 3),
-    "blender": (2, 79, 0),
+    "blender": (2, 80, 0),
     "location": "",
     "warning": "This addon is still in development.",
     "wiki_url": "",
@@ -46,16 +46,4 @@ modules = developer_utils.setup_addon_modules(__path__, __name__, "bpy" in local
 # register
 ##################################
 
-import traceback
-
-def register():
-    try: bpy.utils.register_module(__name__)
-    except: traceback.print_exc()
-
-    print("Registered {} with {} modules".format(bl_info["name"], len(modules)))
-
-def unregister():
-    try: bpy.utils.unregister_module(__name__)
-    except: traceback.print_exc()
-
-    print("Unregistered {}".format(bl_info["name"]))
+register, unregister = bpy.utils.register_classes_factory(__name__)
